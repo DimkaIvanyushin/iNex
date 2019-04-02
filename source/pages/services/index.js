42e0773bd69069547a45c6fd41f515fcc012ee18
Dmitry <dmitry.ivanyushin@gmail.com>
2019-04-02 14:46:36 +0300

    fix menu

--- source/pages/services/index.js
+++ source/pages/services/index.js
@@ -21,5 +21,5 @@ import '../../components/recall/recall.styl'
 import '../../components/per-char/per-char.styl'
 import '../../components/footer-main/footer.styl';
 
-import '../../components/header-menu/header-menu.js';
+// import '../../components/header-menu/header-menu.js';
 new WOW().init();
